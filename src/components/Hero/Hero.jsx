a66b3053b3c61dca391aead2ff643622098b014c
Matthew-Hodgetts <mpolar999@gmail.com>
2021-04-09 20:08:18 +0100

    Hero done

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import "./Hero.scss";
 
+import heroBg from "../../images/hero-bg.jpg";
+import logo from "../../images/logo.png";
+
+
 const Hero = () => {
-    return ( <>hero</> );
+    return ( <div className="hero" style={{backgroundImage: `url(${heroBg})`}}>
+        <img src={logo} alt="Stonefall cottage" className="hero__logo"/>
+    </div> );
 }
  
 export default Hero;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import "./Layout.scss";
+
 const Layout = ({children}) => {
     return ( <main>{children}</main> );
 }
